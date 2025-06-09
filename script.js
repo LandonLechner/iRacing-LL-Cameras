1ae9485f534c7ce8a5c7ad4691480f30fdc23ae0
LandonLechner <landonl64@gmail.com>
2025-06-09 15:03:09 -0400

    Initial commit

--- script.js
+++ script.js
@@ -1,138 +1,62 @@
-window.onload = function ()
-{
-    
-    const proceduresToggle = document.querySelector("#proceduresToggle");
-    const services = document.querySelector("#services");
-    let isProcedures = false;
-    
-    proceduresToggle.addEventListener('click', function() {
-        if (isProcedures) {
-            proceduresToggle.classList.remove("proceduresToggleOn");
-            proceduresToggle.classList.add("proceduresToggleOff");
-            services.style.display = "block";
-            isProcedures = false;
-        } else {
-            proceduresToggle.classList.add("proceduresToggleOn");
-            proceduresToggle.classList.remove("proceduresToggleOff");
-            services.style.display = "none";
-            isProcedures = true;
-        }
-        createEmail();
-        return isProcedures;
-    });
+const camSections = document.querySelectorAll('.cam-section');
+const initial = document.querySelector('#initial');
+const midSections = Array.from(camSections).splice(1).splice(0,5);
+const last = document.querySelector('#last-two');
+const redBlack = document.querySelectorAll('.red-black');
+const yellowBlack = document.querySelectorAll('.yellow-black');
+const whiteBlack = document.querySelectorAll('.white-black');
+const divider = document.querySelectorAll('.divider');
+const gray = "#333";
 
-	document.addEventListener('input', createEmail);
-
-    function createEmail () {
-        const practice = document.querySelector("#practice").value;
-        const client = document.querySelector("#client").value;
-        const closedWonDate = document.querySelector("#closedWonDate").value;
-        const reorderedDate = `${closedWonDate.slice(5,7)}/${closedWonDate.slice(8,10)}/${closedWonDate.slice(2,4)}`;
-        const pm = document.querySelector("#pm").value;
-        const csm = document.querySelector("#csm").value;
-        const procedureQuantity = Number(document.getElementById("serviceQuantity").value);
-        let heshe;
-        let hesheUpper;
-        let himher;
-        
-        if (pm === "Veronica Holt" || pm === "Ruby Blackwood") {
-            heshe = "she";
-            hesheUpper = "She";
-            himher = "her";
-        } else {
-            heshe = "he";
-            hesheUpper = "He";
-            himher = "him";
-        }
-        
-        if (isProcedures) {
-            proceduresLink = "";
-        } else {
-            proceduresLink = `<strong>To prepare for our kickoff call, please complete the 
-            <a target="_blank" rel="noopener noreferrer" href="https://www.docusign.com/blog/types-healthcare-consent-forms">Procedure List Form</a>, 
-            which will guide the creation of your website's service list. 
-            As you select services, keep in mind you are contracted for ${procedureQuantity}. 
-            This will allow us to create a focused and effective service list for your website</strong>.
-            <br><br>`;
-        }
-            
-        let emailSubject = document.querySelector("#emailSubject").innerHTML = 
-            `${practice} - Welcome to MedBest!`;
-        
-        document.querySelector('#copyButton').style.display = "block";
-        document.querySelector('#copyButton2').style.display = "block";
-        
-        let emailBody = document.querySelector("#emailBody").innerHTML =
-            `${client},
-            <br>
-            <br>
-            Congratulations! It's my pleasure to officially welcome you to MedBest!
-            <br>
-            <br>
-            Our team is excited to kick off this project with you.
-            I have copied ${pm} from our Implementation team on this email.
-            ${hesheUpper} will be your main point of contact throughout the 
-            build process of this project and be your go-to person to get your site built and launched.
-            <br>
-            <br>
-            <strong>${pm.split(' ').slice(0, 1)} will be reaching out to you soon to get your kick-off call scheduled.
-            Your 90-day implementation timeline began on ${reorderedDate}, so please work with ${himher}
-            to get that on the calendar as soon as possible.</strong>
-            <br>
-            <br>
-            When you meet with ${pm.split(' ').slice(0, 1)}, ${heshe} will walk you through our implementation process.
-            We expect to have a completed website link ready for you to review no later than 90 days.
-            <br>
-            <br>
-            I would also like to introduce ${csm}, your Customer Success Manager (CSM), 
-            who will work with ${pm.split(' ').slice(0, 1)} during the implementation process and 
-            then transition to your main point of contact once your new site is live. 
-            While ${pm.split(' ').slice(0, 1)} is keeping you up to date on the progress of your new website, 
-            ${csm.split(' ').slice(0, 1)} will be focused on the overall success of your online strategy and 
-            will ensure that your team is maximizing our tools to deliver you the best ROI. 
-            ${csm.split(' ').slice(0, 1)} will also be your go-to point of contact for information on growth accelerators, 
-            best practices, performance reporting, and product updates.
-            <br>
-            <br>
-            ${proceduresLink}
-            Thanks for choosing MedBest. We are proud to partner with you for your digital marketing needs!
-            `;
-    }
-    
-    //copy functionality
-    document.getElementById('copyButton').addEventListener('click', async function() {
-        let subjectElement = document.getElementById('emailSubject');
-        let subjectText = subjectElement.innerText;
-    
-        try {
-            await navigator.clipboard.writeText(subjectText);
-    
-            document.querySelector('#copyButton').innerText = "Copied!";
-            setTimeout(() => {
-                document.querySelector('#copyButton').innerText = "Copy Subject Line";
-            }, 1300);
-        } catch (err) {
-            console.error("Failed to copy:", err);
-        }
-    });
-
-    document.getElementById('copyButton2').addEventListener('click', async function() {
-        let emailBody = document.getElementById('emailBody');
-    
-        const blob = new Blob([emailBody.innerHTML], { type: 'text/html' });
-        const data = [new ClipboardItem({ 'text/html': blob })];
-    
-        try {
-            await navigator.clipboard.write(data);
-    
-            document.querySelector('#copyButton2').innerText = "Copied!";
-            
-            setTimeout(() => {
-                document.querySelector('#copyButton2').innerText = "Copy Email Body";
-            }, 1300);
-        } catch (err) {
-            console.error("Failed to copy:", err);
-        }
-    });
-
+const colorHelper = (elements, prop, color) => {
+    elements.forEach(el => el.style[prop] = color);
 };
+
+const goGrayAll = () => {
+    [camSections, redBlack, yellowBlack, whiteBlack].forEach(el =>
+        colorHelper(el, "color", gray))
+    colorHelper(divider, "backgroundColor", gray);
+    camSections.forEach(el => colorHelper(el.querySelectorAll('li'), "color", gray))
+    camSections.forEach(el => 
+        colorHelper(el.querySelectorAll('span:not(.cam-name)'), "border", "1px solid rgba(255, 255, 255, 0.2)"));
+};
+
+const resetColors = () => {
+    camSections.forEach(el => colorHelper(el.querySelectorAll('li'), "color", "#ddd"))
+    colorHelper(redBlack, "color", "red")
+    colorHelper(yellowBlack, "color", "#bbbb00")
+    colorHelper(whiteBlack, "color", "#ddd")
+    colorHelper(divider, "backgroundColor", "#888")
+    colorHelper(document.querySelectorAll('.framed'), "border", "1px solid #888")
+};
+
+const focused = (el) => {
+    goGrayAll();
+    const containedRedBlack = el.querySelectorAll('.red-black')
+    if (containedRedBlack.length > 1) {
+        colorHelper(containedRedBlack, "color", "red");
+    } else {
+        el.querySelector('.red-black').style.color = "red";
+    }
+    colorHelper(el.querySelectorAll('li'), "color", "#ddd");
+    colorHelper(el.querySelectorAll('.white-black'), "color", "#ddd");    
+    colorHelper(divider, "backgroundColor", "#333");
+    el.querySelectorAll('span:not(.cam-name)').forEach(span => span.style.border = "1px solid #888");
+    const containedYellowBlack = el.querySelectorAll('.yellow-black')
+    if (containedYellowBlack.length > 1) {
+        colorHelper(containedYellowBlack, "color", "#bbbb00");
+    } else if (containedYellowBlack.length === 1) {
+        el.querySelector('.yellow-black').style.color = "#bbbb00";
+    } else {
+        return;
+    }
+}
+
+camSections.forEach(el => {
+    ['mouseleave', 'focusout'].forEach(e => 
+        el.addEventListener(e, resetColors)
+    );
+    ['mouseenter', 'focusin'].forEach(e =>
+    el.addEventListener(e, () => focused(el))
+    );
+});
