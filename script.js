c22512173300b7c30a14eb936839c41f1dd7fe95
LandonLechner <landonl64@gmail.com>
2026-02-19 15:31:30 -0500

    Updated LL_Drone and LL_Top settings, reduced max-width

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ setupCopyElement(msg1, msg1Text);
 setupCopyElement(msg2, msg2Text);
 setupCopyElement(msg3, msg3Text);
 
+
+
+
 const preRace = document.querySelector('#pre-race');
 const race = document.querySelector('#race');
 const mc1 = document.querySelector('.main-container1');
@@ -63,73 +66,6 @@ race.addEventListener('change', function() {
     }
 });
 
-// const camSections = document.querySelectorAll('.cam-section');
-// const initial = document.querySelector('#initial');
-// const midSections = Array.from(camSections).slice(1, 8);
-// const last = document.querySelector('#last-two');
-// const whiteBlack = document.querySelectorAll('.white-black');
-// const divider = document.querySelectorAll('.divider');
-// const gray = "#333";
-// const tab = document.querySelectorAll('.tab');
-
-// const colorHelper = (elements, prop, color) => {
-//     elements.forEach(el => el.style[prop] = color);
-// };
-
-
-// const goGrayAll = () => {
-//     [camSections, whiteBlack].forEach(el =>
-//         colorHelper(el, "color", gray))
-//     colorHelper(divider, "backgroundColor", gray);
-//     camSections.forEach(el => {
-//         colorHelper(el.querySelectorAll('li'), "color", gray)
-//         colorHelper(el.querySelectorAll('.framed'), "border", "1px solid rgba(255, 255, 255, 0.2)");
-//         colorHelper(el.querySelectorAll('.tab'), "text-shadow", "none");
-//     });
-
-//     camSections.forEach(el => colorHelper(el.querySelectorAll('.tab'), "border-bottom", "2px solid rgba(25, 117, 255, 0)"))
-// };
-
-// const resetColors = () => {
-//     camSections.forEach(el => {
-//         colorHelper(el.querySelectorAll('li'), "color", "#ddd");
-//         colorHelper(el.querySelectorAll('.framed'), "border", "1px solid rgba(255, 255, 255, 0.2)");
-//         colorHelper(el.querySelectorAll('.tab'), "border-bottom", "2px solid rgba(25, 117, 255, 1)");
-//         colorHelper(el.querySelectorAll('.tab'), "text-shadow", "0 4px 4px rgb(25, 117, 255)");
-//     });
-//     colorHelper(whiteBlack, "color", "#ddd");
-//     colorHelper(divider, "backgroundColor", "#888");
-//     colorHelper(document.querySelectorAll('.framed'), "border", "1px solid #888");
-// };
-
-// const focused = (el) => {
-//     goGrayAll();
-    
-//     const highlight = [
-//         { selector: '.white-black', color: '#ddd' },
-//     ];
-    
-//     highlight.forEach(({ selector, color }) => {
-//         const matches = el.querySelectorAll(selector);
-//         if (matches.length) colorHelper(matches, 'color', color);
-//     });
-    
-//     colorHelper(el.querySelectorAll('li'), "color", "#ddd");
-//     colorHelper(divider, "backgroundColor", gray);
-//     colorHelper(el.querySelectorAll('.framed'), "border", "1px solid #888");
-//     colorHelper(el.querySelectorAll('.tab'), "border-bottom", "2px solid rgba(25, 117, 255, 1)");
-//     colorHelper(el.querySelectorAll('.tab'), "text-shadow", "0 4px 4px rgb(25, 117, 255)");
-// };
-
-// camSections.forEach(el => {
-//     ['mouseleave', 'focusout'].forEach(e => 
-//         el.addEventListener(e, resetColors)
-        
-//     );
-//     ['mouseenter', 'focusin'].forEach(e =>
-//     el.addEventListener(e, () => focused(el))
-//     );
-// });
 
 
 const camSections = document.querySelectorAll('.cam-section');
